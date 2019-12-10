d7dfe0cdc0088276035cc26d9f01af8cf0582d5c
Johnson Denen <johnson.denen@gmail.com>
2019-12-10 14:28:17 -0500

    Refactor Dataset and Dataset.Schema structure

--- apps/dataset/lib/dataset.ex
+++ apps/dataset/lib/dataset.ex
@@ -1,10 +1,8 @@
 defmodule Dataset do
-  @moduledoc "TODO"
-  @version 1
+  @type t() :: %__MODULE__{}
+  @schema Application.get_env(:dataset, :schema)
 
-  import Norm
-
-  defstruct version: @version,
+  defstruct version: nil,
             id: nil,
             org_id: nil,
             created_ts: nil,
@@ -17,31 +15,12 @@ defmodule Dataset do
             boundaries: %{spatial: [], temporal: []},
             data: []
 
-  @type t() :: %__MODULE__{}
-
   @spec new(map()) :: t()
   def new(%{} = input) do
     map = for {key, val} <- input, do: {:"#{key}", val}, into: %{}
 
     struct(__MODULE__, map)
-    |> conform(s())
+    |> Norm.conform(@schema.s())
   end
 
-  @spec s() :: %Norm.Schema{}
-  def s do
-    schema(%__MODULE__{
-      version: spec(fn v -> v == @version end),
-      id: Dataset.Schema.string(),
-      org_id: Dataset.Schema.string(),
-      title: Dataset.Schema.string(),
-      description: spec(is_binary()),
-      keywords: spec(is_list()),
-      license: Dataset.Schema.string(),
-      created_ts: Dataset.Schema.timestamp(),
-      modified_ts: Dataset.Schema.timestamp(),
-      contact: Dataset.Schema.contact(),
-      boundaries: Dataset.Schema.boundaries(),
-      data: spec(is_list())
-    })
-  end
 end
