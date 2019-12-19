b6e55d3d5c982e2392fbf03d990b9cf0da8ff7fd
Johnson Denen <johnson.denen@gmail.com>
2019-12-19 13:28:59 -0500

    Make spec documentation more consistent
    Run formatter

--- apps/definition_faker/lib/definition_faker.ex
+++ apps/definition_faker/lib/definition_faker.ex
@@ -1,14 +1,14 @@
 defmodule DefinitionFaker do
   alias Definition.{Dataset, Owner}
 
-  @spec dataset(map()) :: Dataset.t()
+  @spec dataset(override :: map) :: Dataset.t()
   def dataset(override) do
     DefinitionFaker.Dataset.default()
     |> Map.merge(override)
     |> Dataset.new()
   end
 
-  @spec owner(map()) :: Owner.t()
+  @spec owner(override :: map) :: Owner.t()
   def owner(override) do
     DefinitionFaker.Owner.default()
     |> Map.merge(override)
--- apps/definition_faker/mix.exs
+++ apps/definition_faker/mix.exs
@@ -26,7 +26,7 @@ defmodule DefinitionFaker.MixProject do
   defp deps do
     [
       {:faker, "~> 0.12"},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
     ]
   end
 end
