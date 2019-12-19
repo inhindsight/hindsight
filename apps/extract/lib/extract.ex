4c53d3209eca7b478c6deee0c1f3c140faa07614
Brian Balser <bbalser@pillartechnology.com>
2019-12-19 09:38:33 -0500

    Create extract library which defines extract step protocol and extract execute_steps

---
+++ apps/extract/lib/extract.ex
@@ -0,0 +1,63 @@
+defmodule Extract do
+  defmodule Context do
+    @type t() :: %__MODULE__{
+            response: Tesla.Env.t(),
+            variables: map,
+            stream: Stream.t()
+          }
+    defstruct response: nil, variables: %{}, stream: nil
+
+    @spec new() :: t
+    def new() do
+      %Context{}
+    end
+
+    @spec set_response(t, Tesla.Env.t()) :: t
+    def set_response(context, response) do
+      Map.put(context, :response, response)
+    end
+
+    @spec add_variable(t, String.t(), String.t()) :: t
+    def add_variable(context, name, value) do
+      new_variables = Map.put(context.variables, name, value)
+      Map.put(context, :variables, new_variables)
+    end
+
+    @spec set_stream(t, Stream.t()) :: t
+    def set_stream(context, stream) do
+      Map.put(context, :stream, stream)
+    end
+  end
+
+  defprotocol Step do
+    @spec execute(t, Context.t()) :: Context.t()
+    def execute(step, context)
+  end
+
+  @spec execute_steps(list) :: {:ok, Stream.t} | {:error, term}
+  def execute_steps(steps) do
+    context =
+      steps
+      |> parse_steps()
+      |> Enum.reduce(Context.new(), &Extract.Step.execute/2)
+
+    {:ok, context.stream}
+  rescue
+    e -> {:error, error_message(e)}
+  end
+
+  defp parse_steps(steps) do
+    steps
+    |> Enum.map(fn step ->
+      struct = :"Elixir.#{Map.get(step, :step)}"
+      struct!(struct, Map.delete(step, :step))
+    end)
+  end
+
+  defp error_message(e) do
+    case Exception.exception?(e) do
+      true -> Exception.message(e)
+      false -> e
+    end
+  end
+end
---
+++ apps/extract/mix.exs
@@ -0,0 +1,33 @@
+defmodule Extract.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :extract,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+end
