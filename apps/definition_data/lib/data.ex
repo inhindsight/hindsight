aba7a12ce68f1279983d1a59a557da43c73db65e
Johnson Denen <johnson.denen@gmail.com>
2019-12-19 16:41:15 -0500

    Add definition_data app

---
+++ apps/definition_data/lib/data.ex
@@ -0,0 +1,18 @@
+defmodule Data do
+  @moduledoc """
+  Documentation for Data.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Data.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
