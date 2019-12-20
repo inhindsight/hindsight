b68cde818e103146b6409481264abd3238e0b563
Brian Balser <bbalser@pillartechnology.com>
2019-12-20 13:18:13 -0500

    Update apps/extract_decode_csv/lib/decode/csv.ex
    Co-Authored-By: Johnson Denen <johnson.denen@gmail.com>

--- apps/extract_decode_csv/lib/decode/csv.ex
+++ apps/extract_decode_csv/lib/decode/csv.ex
@@ -18,7 +18,8 @@ defmodule Decode.Csv do
         context.stream
         |> Stream.flat_map(&String.split(&1, "\n"))
         |> Decode.Csv.Parser.parse_stream(skip_headers: step.skip_first_line)
-        |> Stream.map(fn row -> Enum.zip(step.headers, row) |> Map.new() end)
+        |> Stream.map(&Enum.zip(step.headers, &1))
+        |> Stream.map(&Map.new/1)
 
       {:ok, set_stream(context, new_stream)}
     end
