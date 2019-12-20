67e041bd6e10440bea47ce4e2d5876905dee4ccd
Johnson Denen <johnson.denen@gmail.com>
2019-12-20 11:55:28 -0500

    Fix test compilation issue

--- apps/definition_faker/lib/definition_faker.ex
+++ apps/definition_faker/lib/definition_faker.ex
@@ -1,28 +1,22 @@
 defmodule DefinitionFaker do
-  if Code.ensure_loaded?(Dataset) do
-    @spec dataset(override :: map) :: Dataset.t()
-    def dataset(override) do
-      DefinitionFaker.Dataset.default()
-      |> Map.merge(override)
-      |> Dataset.new()
-    end
+  @spec dataset(override :: map) :: Dataset.t()
+  def dataset(override) do
+    DefinitionFaker.Dataset.default()
+    |> Map.merge(override)
+    |> Dataset.new()
   end
 
-  if Code.ensure_loaded?(Dataset.Owner) do
-    @spec owner(override :: map) :: Dataset.Owner.t()
-    def owner(override) do
-      DefinitionFaker.Owner.default()
-      |> Map.merge(override)
-      |> Dataset.Owner.new()
-    end
+  @spec owner(override :: map) :: Dataset.Owner.t()
+  def owner(override) do
+    DefinitionFaker.Owner.default()
+    |> Map.merge(override)
+    |> Dataset.Owner.new()
   end
 
-  if Code.ensure_loaded?(Data) do
-    @spec data(override :: map) :: Data.t()
-    def data(override) do
-      DefinitionFaker.Data.default()
-      |> Map.merge(override)
-      |> Data.new()
-    end
+  @spec data(override :: map) :: Data.t()
+  def data(override) do
+    DefinitionFaker.Data.default()
+    |> Map.merge(override)
+    |> Data.new()
   end
 end
