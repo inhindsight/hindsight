5e5414e017c13ed91f338243943490862f52aadc
Johnson Denen <johnson.denen@gmail.com>
2019-12-23 12:50:24 -0500

    Add definition_schedule app

---
+++ apps/definition_schedule/lib/schedule.ex
@@ -0,0 +1,18 @@
+defmodule Schedule do
+  @moduledoc """
+  Documentation for Schedule.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Schedule.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
