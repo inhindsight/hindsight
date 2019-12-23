50bb17b78c5620b9892802382ed5d48b38774ba4
Brian Balser <brian.balser@gmail.com>
2019-12-23 10:04:07 -0500

    Transform.Steps

---
+++ apps/transform_steps/README.md
@@ -0,0 +1,21 @@
+# LoadSteps
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `transform_steps` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:transform_steps, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/transform_steps](https://hexdocs.pm/transform_steps).
+
--- apps/transform_steps/mix.exs
+++ apps/transform_steps/mix.exs
@@ -1,15 +1,15 @@
-defmodule Extract.Steps.MixProject do
+defmodule TransformSteps.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :extract_steps,
+      app: :transform_steps,
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -17,19 +17,15 @@ defmodule Extract.Steps.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ok, in_umbrella: true},
-      {:definition, in_umbrella: true},
-      {:tesla, github: "teamon/tesla", branch: "master"},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
     ]
   end
