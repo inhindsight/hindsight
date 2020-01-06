e58286626f4ced0463054a894099245801a4eb3c
Brian Balser <brian.balser@gmail.com>
2020-01-05 19:43:36 -0500

    Created writer behaviour and kafka topic writer

---
+++ apps/writer/mix.exs
@@ -0,0 +1,27 @@
+defmodule Writer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :writer,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
