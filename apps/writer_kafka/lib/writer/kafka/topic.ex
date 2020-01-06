2618d7d05c8e92efb240388c5a28a316f9d4723c
Brian Balser <bbalser@pillartechnology.com>
2020-01-05 19:43:36 -0500

    Created writer behaviour and kafka topic writer

---
+++ apps/writer_kafka/lib/writer/kafka/topic.ex
@@ -0,0 +1,62 @@
+defmodule Writer.Kafka.Topic do
+  @behaviour Writer
+  use GenServer
+  use Retry
+
+  defmodule State do
+    defstruct [:connection, :endpoints, :topic, :elsa_sup]
+  end
+
+  def start_link(args) do
+    server_opts = [name: Keyword.get(args, :name, nil)]
+    GenServer.start_link(__MODULE__, args, server_opts)
+  end
+
+  def write(server, messages, opts \\ []) do
+    GenServer.call(server, {:write, messages, opts})
+  end
+
+  def init(opts) do
+    state = %State{
+      connection: Keyword.get(opts, :connection, default_connection_name()),
+      endpoints: Keyword.fetch!(opts, :endpoints),
+      topic: Keyword.fetch!(opts, :topic)
+    }
+
+    unless Elsa.topic?(state.endpoints, state.topic) do
+      create_topic(state.endpoints, state.topic)
+    end
+
+    {:ok, elsa_sup} =
+      Elsa.Supervisor.start_link(
+        connection: state.connection,
+        endpoints: state.endpoints,
+        producer: [
+          topic: state.topic
+        ]
+      )
+
+    {:ok, %{state | elsa_sup: elsa_sup}}
+  end
+
+  def handle_call({:write, messages, _opts}, _from, state) do
+    Elsa.produce(state.connection, state.topic, messages)
+    |> reply(state)
+  end
+
+  defp create_topic(endpoints, topic) do
+    Elsa.create_topic(endpoints, topic)
+
+    wait exponential_backoff(100) |> Stream.take(10) do
+      Elsa.topic?(endpoints, topic)
+    after
+      _ -> :ok
+    else
+      _ -> raise "Timed out waiting for #{topic} to be available"
+    end
+  end
+
+  defp reply(message, state), do: {:reply, message, state}
+
+  defp default_connection_name(), do: :"#{__MODULE__}_#{inspect(self())}"
+end
---
+++ apps/writer_kafka/test/writer/kafka/topic_test.exs
@@ -0,0 +1,24 @@
+defmodule Writer.Kafka.TopicTest do
+  use ExUnit.Case
+  use Divo
+  import AssertAsync
+
+  alias Writer.Kafka.Topic
+
+  @server [localhost: 9092]
+
+  test "topic writer will create topic and produce messages" do
+    {:ok, writer} =
+      Topic.start_link(
+        endpoints: @server,
+        topic: "topic-435"
+      )
+
+    :ok = Topic.write(writer, ["message1"])
+
+    assert_async debug: true do
+      {:ok, _count, messages} = Elsa.fetch(@server, "topic-435")
+      assert Enum.any?(messages, &match?(%{value: "message1"}, &1))
+    end
+  end
+end
