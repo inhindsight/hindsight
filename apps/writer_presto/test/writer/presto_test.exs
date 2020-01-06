e5b50eca0c5c20da405a7c6efdb62ba63c59559e
Brian Balser <brian.balser@gmail.com>
2020-01-06 11:30:00 -0500

    Handle errors in prest writer

--- apps/writer_presto/test/writer/presto_test.exs
+++ apps/writer_presto/test/writer/presto_test.exs
@@ -1,58 +1,43 @@
 defmodule Writer.PrestoTest do
   use ExUnit.Case
-  use Divo
-
-  @moduletag integration: true, divo: true, capture_log: true
+  use Placebo
 
   alias Writer.Presto
 
-  @url "http://localhost:8080"
+  setup do
+    Process.flag(:trap_exit, true)
+    :ok
+  end
 
-  test "something, something, presto" do
-    table_schema = [
-      {"name", "varchar"},
-      {"age", "integer"},
-      {"colors", "array(varchar)"},
-      {"spouse", "row(name varchar, age integer, colors array(varchar))"}
-    ]
+  test "process should die if unable to create table" do
+    allow Prestige.execute(any(), starts_with("CREATE TABLE")),
+      return: {:error, "unable to create table"}
+
+    result =
+      Presto.start_link(
+        url: "http://some.url",
+        user: "testing",
+        table_schema: [{"name", "varchar"}],
+        table: "table1"
+      )
+
+    assert result == {:error, "unable to create table"}
+  end
+
+  test "write should return error tuple if unable to write messages" do
+    allow Prestige.execute(any(), starts_with("CREATE TABLE")),
+      return: {:ok, :result}
+
+    allow Prestige.execute(any(), starts_with("INSERT INTO")), return: {:error, "failed to query"}
 
     {:ok, pid} =
       Presto.start_link(
-        url: @url,
+        url: "http://some.url",
         user: "testing",
-        table: "table1",
-        table_schema: table_schema,
-        catalog: "hive",
-        schema: "default"
+        table_schema: [{"name", "varchar"}],
+        table: "table1"
       )
 
-    Presto.write(pid, [
-      %{
-        "name" => "george",
-        "age" => 21,
-        "colors" => ["red", "blue"],
-        "spouse" => [{"name", "shirley"}, {"age", 23}, {"colors", ["yellow","green"]}]
-      }
-    ])
-
-    session =
-      Prestige.new_session(
-        url: @url,
-        user: "testing",
-        catalog: "hive",
-        schema: "default"
-      )
-
-    expected = [
-      %{
-        "name" => "george",
-        "age" => 21,
-        "colors" => ["red", "blue"],
-        "spouse" => %{"name" => "shirley", "age" => 23, "colors" => ["yellow", "green"]}
-      }
-    ]
-
-    result = Prestige.query!(session, "select * from table1")
-    assert expected == Prestige.Result.as_maps(result)
+    assert {:error, "failed to query"} == Presto.write(pid, [%{"name" => "bob"}])
   end
 end
