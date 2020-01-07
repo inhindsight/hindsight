95a0345a368a32010f22919c33eff40dab6c4c8f
Johnson Denen <johnson.denen@gmail.com>
2020-01-07 16:15:10 -0500

    definition_dictionary uses Definition.Schema.Type functions

--- apps/definition_dictionary/lib/dictionary/type/list.ex
+++ apps/definition_dictionary/lib/dictionary/type/list.ex
@@ -77,9 +77,9 @@ defmodule Dictionary.Type.List.V1 do
 
   def s do
     schema(%Dictionary.Type.List{
-      version: spec(fn v -> v == 1 end),
-      name: spec(is_binary() and not_empty?()),
-      description: spec(is_binary()),
+      version: version(1),
+      name: required_string(),
+      description: string(),
       item_type: spec(is_atom() and not_nil?()),
       fields: spec(is_list())
     })
