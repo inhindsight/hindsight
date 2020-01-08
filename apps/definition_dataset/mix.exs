d9fac3053c20e2eacac5ab4091fd6aa40366a290
Johnson Denen <johnson.denen@gmail.com>
2020-01-08 12:37:56 -0500

    All Elixir versions are 1.9

--- apps/definition_dataset/mix.exs
+++ apps/definition_dataset/mix.exs
@@ -9,7 +9,7 @@ defmodule Dataset.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
