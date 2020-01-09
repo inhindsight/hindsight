a81e49b5f05a7406241343943b5086dfac77d2f8
Johnson Denen <johnson.denen@gmail.com>
2020-01-09 10:16:51 -0500

    Add comment documentation in config/releases.exs
    This pattern is a little different, so some explanation may be worth it

--- config/releases.exs
+++ config/releases.exs
@@ -1,1 +1,15 @@
 import Config
+#
+# Both compilation and runtime configuration should be set here across all apps.
+# Non-prod specific environment configuration should reside in an app's config
+# file for that Mix environment. For example: apps/my_app/config/test.exs.
+#
+# Configuration accessing environment variables should ALWAYS set a default,
+# as this configuration will ALWAYS be evaluated.
+#
+# Example:
+#
+#     config :my_app, :some_key,
+#       abc: 123,
+#       foo: System.get_env("FOO", "bar:baz")
+#
