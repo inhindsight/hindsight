f4ab54b174d792940d4f6bb0adacba632a5f01ca
Johnson Denen <johnson.denen@gmail.com>
2020-01-09 16:21:20 -0500

    Use Divo to manage external dependencies

--- apps/platform_runner/mix.exs
+++ apps/platform_runner/mix.exs
@@ -25,9 +25,8 @@ defmodule PlatformRunner.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:service_gather, in_umbrella: true},
+      {:divo, "~> 1.1"}
     ]
   end
 end
