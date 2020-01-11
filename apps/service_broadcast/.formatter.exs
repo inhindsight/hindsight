90b45df8048d400980050f8f3046f0b8b20d2adf
Brian Balser <brian.balser@gmail.com>
2020-01-11 08:43:00 -0500

    initial service_broadcast

---
+++ apps/service_broadcast/.formatter.exs
@@ -0,0 +1,4 @@
+[
+  import_deps: [:phoenix, :checkov],
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
+]
---
+++ apps/service_broadcast/lib/broadcast/application.ex
@@ -0,0 +1,29 @@
+defmodule Broadcast.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the endpoint when the application starts
+      BroadcastWeb.Endpoint
+      # Starts a worker by calling: Broadcast.Worker.start_link(arg)
+      # {Broadcast.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Broadcast.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    BroadcastWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
