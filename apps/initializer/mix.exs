bc65849a52da622d501ebc2e3b0cb1c4bcade55a
Brian Balser <brian.balser@gmail.com>
2020-01-12 18:09:23 -0500

    Wroter initializer and converted broadcast init to it.

---
+++ apps/initializer/mix.exs
@@ -0,0 +1,29 @@
+defmodule Initializer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :initializer,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:retry, "~> 0.13.0"}
+    ]
+  end
+end
