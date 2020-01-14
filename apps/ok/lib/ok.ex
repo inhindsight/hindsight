3c2f24b6315da5f71e4822dacf32ac52205d45e4
jeffgrunewald <jeff@grunewalddesign.com>
2020-01-14 14:17:01 -0500

    address pr feedback

--- apps/ok/lib/ok.ex
+++ apps/ok/lib/ok.ex
@@ -26,8 +26,8 @@ defmodule Ok do
   @spec reduce(
           Enum.t(),
           Enum.acc(),
-          (Enum.element(), Enum.acc() -> {:ok, Enum.acc()} | error)
-        ) :: {:ok, Enum.acc()} | error
+          (Enum.element(), Enum.acc() -> {:ok, Enum.acc()} | {:error, reason})
+        ) :: {:ok, Enum.acc()} | {:error, reason} when reason: term
   def reduce(enum, initial, function) do
     Enum.reduce_while(enum, {:ok, initial}, fn item, {:ok, acc} ->
       case function.(item, acc) do
@@ -53,8 +53,8 @@ defmodule Ok do
 
   def each({:error, _} = error), do: error
 
-  @spec transform(Enum.t(), (Enum.element() -> {:ok, Enum.element()} | error)) ::
-          {:ok, Enum.t()} | error
+  @spec transform(Enum.t(), (Enum.element() -> {:ok, Enum.element()} | {:error, reason})) ::
+          {:ok, Enum.t()} | {:error, reason} when reason: term
   def transform(enum, function) when is_list(enum) and is_function(function, 1) do
     reduce(enum, [], fn item, acc ->
       function.(item)
