01d126c88f6a51c9c0e1fbdfb166d9e05fed331a
Brian Balser <brian.balser@gmail.com>
2020-01-15 11:29:47 -0500

    Moved assert_async and temp_env under new testing app

--- apps/testing/mix.exs
+++ apps/testing/mix.exs
@@ -1,9 +1,9 @@
-defmodule AssertAsync.MixProject do
+defmodule Testing.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :assert_async,
+      app: :testing,
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
@@ -11,20 +11,23 @@ defmodule AssertAsync.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      dialyzer: [
-        plt_add_apps: [:ex_unit]
-      ]
+      deps: deps()
     ]
   end
 
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
+  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      # {:sibling_app_in_umbrella, in_umbrella: true}
+    ]
   end
 end
