6baf0b3521781f6bd0989714695cc883143d6dc4
Brian Balser <bbalser@pillartechnology.com>
2020-01-17 12:40:10 -0500

    Added type spec to Schedule Struct

--- apps/definition_schedule/lib/schedule.ex
+++ apps/definition_schedule/lib/schedule.ex
@@ -1,6 +1,18 @@
 defmodule Schedule do
   use Definition, schema: Schedule.V1
 
+  @type uuid :: String.t()
+
+  @type t :: %__MODULE__{
+    version: integer,
+    id: uuid,
+    dataset_id: uuid,
+    cron: String.t(),
+    extract: Extract.t(),
+    transform: Transform.t(),
+    load: list
+  }
+
   defstruct version: 1,
             id: nil,
             dataset_id: nil,
