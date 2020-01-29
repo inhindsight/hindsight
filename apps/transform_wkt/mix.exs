c2073431e06cd7267836e02f622130ade4e898ab
Brian Balser <brian.balser@gmail.com>
2020-01-29 13:21:00 -0500

    Created Transformer.Wkt.Point

---
+++ apps/transform_wkt/mix.exs
@@ -0,0 +1,31 @@
+defmodule Transform.WKT.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :transform_wkt,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:transformer, in_umbrella: true},
+      {:definition_dictionary, in_umbrella: true},
+      {:geo, "~> 3.3"}
+    ]
+  end
+end
