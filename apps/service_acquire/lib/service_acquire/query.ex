40a5d89078724fe8944b55e3c2fb52e5142dad31
Johnson Denen <johnson.denen@gmail.com>
2020-01-29 10:57:05 -0500

    First pass at boundary limited queries

--- apps/service_acquire/lib/service_acquire/query.ex
+++ apps/service_acquire/lib/service_acquire/query.ex
@@ -7,10 +7,10 @@ defmodule Acquire.Query do
   def from_params(%{"dataset" => dataset} = params) do
     subset = Map.get(params, "subset", "default")
     fields = Map.get(params, "fields", "*")
-    {filter, values} = filter(params)
+    {where, values} = parse_where(params)
 
     statement =
-      ["SELECT", fields, "FROM #{dataset}__#{subset}", filter, limit(params)]
+      ["SELECT", fields, "FROM #{dataset}__#{subset}", where, limit(params)]
       |> Enum.filter(& &1)
       |> Enum.join(" ")
       |> String.trim()
@@ -18,16 +18,48 @@ defmodule Acquire.Query do
     {statement, values}
   end
 
+  defp parse_where(params) do
+    {filter, filter_values} = filter(params)
+    {boundary, boundary_values} = boundary(params)
+
+    {where_clause(filter, boundary), filter_values ++ boundary_values}
+  end
+
+  defp where_clause([], []), do: ""
+
+  defp where_clause(filter, boundary) do
+    clause =
+      filter ++ boundary
+      |> Enum.join(" AND ")
+
+    "WHERE #{clause}"
+  end
+
+  defp boundary(%{"boundary" => boundary}) do
+    bbox =
+      String.split(boundary, ",", trim: true)
+      |> Enum.map(&String.to_float/1)
+
+    input = "ST_LineString(array[ST_Point(?, ?), ST_Point(?, ?)])"
+    envelope = "ST_Envelope(#{input})"
+    # TODO get WKT field dynamically
+    wkt = "ST_GeometryFromText(__wkt__)"
+
+    {["ST_Contains(#{envelope}, #{wkt})"], bbox}
+  end
+
+  defp boundary(_), do: {[], []}
+
   defp filter(%{"filter" => filters}) do
     {keys, vals} =
       String.split(filters, ",", trim: true)
       |> Enum.map(fn filter -> String.split(filter, "=", trim: true) end)
       |> Enum.map_reduce([], fn [key, val], acc -> {"#{key}=?", [val | acc]} end)
 
-    {"WHERE " <> Enum.join(keys, " AND "), Enum.reverse(vals)}
+    {keys, Enum.reverse(vals)}
   end
 
-  defp filter(_), do: {nil, []}
+  defp filter(_), do: {[], []}
 
   defp limit(%{"limit" => limit}), do: "LIMIT #{limit}"
   defp limit(_), do: nil
--- apps/service_acquire/test/service_acquire/query_test.exs
+++ apps/service_acquire/test/service_acquire/query_test.exs
@@ -12,7 +12,8 @@ defmodule Acquire.QueryTest do
         [%{"fields" => "c,d"}, {"SELECT c,d FROM a__b", []}],
         [%{"filter" => "c=1"}, {"SELECT * FROM a__b WHERE c=?", ["1"]}],
         [%{"filter" => "c=1,d=2"}, {"SELECT * FROM a__b WHERE c=? AND d=?", ["1", "2"]}],
-        [%{"limit" => "10"}, {"SELECT * FROM a__b LIMIT 10", []}]
+        [%{"limit" => "10"}, {"SELECT * FROM a__b  LIMIT 10", []}],
+        [%{"boundary" => "1.0,1.0,2.0,2.0"}, {"SELECT * FROM a__b WHERE ST_Contains(ST_Envelope(ST_LineString(array[ST_Point(?, ?), ST_Point(?, ?)])), ST_GeometryFromText(__wkt__))", [1.0, 1.0, 2.0, 2.0]}]
       ]
     end
   end
