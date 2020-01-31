7b45003bf7e69b9e598852fa9dd2af78e554386f
Brian Balser <brian.balser@gmail.com>
2020-01-31 08:56:55 -0500

    Broadcast: send anything in the cache to client when the join a channel

--- apps/service_broadcast/lib/broadcast_web/channels/broadcast.ex
+++ apps/service_broadcast/lib/broadcast_web/channels/broadcast.ex
@@ -1,11 +1,25 @@
 defmodule BroadcastWeb.Channel do
-  use Phoenix.Channel
+  use BroadcastWeb, :channel
 
-  def join("broadcast:" <> _topic, _message, socket) do
+  @update_event "update"
+
+  def join("broadcast:" <> topic, _message, socket) do
+    send(self(), {:after_join, topic})
     {:ok, socket}
   end
 
   def handle_in(_type, _message, socket) do
     {:noreply, socket}
+  end
+
+  def handle_info({:after_join, topic}, socket) do
+    Broadcast.Cache.Registry.via(topic)
+    |> Broadcast.Cache.get()
+    |> Enum.map(&push(socket, @update_event, &1))
+
+    {:noreply, socket}
+  catch
+    :exit, _ ->
+      {:noreply, socket}
   end
 end
