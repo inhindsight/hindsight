c5eaa9704883faf05c7670b0313c0cc460827508
Johnson Denen <johnson.denen@gmail.com>
2020-02-03 12:42:15 -0500

    Add ST.Envelope module

---
+++ apps/service_acquire/test/service_acquire/query/st/envelope_test.exs
@@ -0,0 +1,18 @@
+defmodule Acquire.Query.ST.EnvelopeTest do
+  use ExUnit.Case
+
+  alias Acquire.Query.ST.Envelope
+  alias Acquire.Query.{Function, Parameter}
+  alias Acquire.Queryable
+
+  describe "parsing" do
+    test "parses ST_Envelope" do
+      fun = Function.new!(function: "foo", args: ["bar", Parameter.new!(value: 42)])
+      envelope = Envelope.new!(geometry: fun)
+
+      assert Queryable.parse_statement(envelope) == "ST_Envelope(foo(bar, ?))"
+      assert Queryable.parse_input(envelope) == [42]
+    end
+  end
+
+end
