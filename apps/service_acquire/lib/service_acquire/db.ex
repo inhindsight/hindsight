0945ce84e2d4397fc458d3c0cbaed1324b6da882
Johnson Denen <johnson.denen@gmail.com>
2020-02-03 17:07:09 -0500

    Move Acquire.Db.Presto into its own file

--- apps/service_acquire/lib/service_acquire/db.ex
+++ apps/service_acquire/lib/service_acquire/db.ex
@@ -7,21 +7,3 @@ defmodule Acquire.Db do
 
   @callback execute(statement, list) :: Ok.result()
 end
-
-defmodule Acquire.Db.Presto do
-  # TODO
-  @moduledoc false
-
-  use Properties, otp_app: :service_acquire
-  getter(:presto, required: true)
-
-  @behaviour Acquire.Db
-
-  @impl true
-  def execute(template, values) do
-    presto()
-    |> Prestige.new_session()
-    |> Prestige.query(template, values)
-    |> Ok.map(&Prestige.Result.as_maps/1)
-  end
-end
---
+++ apps/service_acquire/lib/service_acquire/db/presto.ex
@@ -0,0 +1,17 @@
+defmodule Acquire.Db.Presto do
+  # TODO
+  @moduledoc false
+
+  use Properties, otp_app: :service_acquire
+  getter(:presto, required: true)
+
+  @behaviour Acquire.Db
+
+  @impl true
+  def execute(template, values) do
+    presto()
+    |> Prestige.new_session()
+    |> Prestige.query(template, values)
+    |> Ok.map(&Prestige.Result.as_maps/1)
+  end
+end
