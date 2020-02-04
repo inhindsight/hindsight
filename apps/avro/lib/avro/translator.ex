669f1130a4eadf814bc14f02a5179df86b19c704
Brian Balser <brian.balser@gmail.com>
2020-02-04 09:51:47 -0500

    avro: translation for Dictionary.Type.List

--- apps/avro/lib/avro/translator.ex
+++ apps/avro/lib/avro/translator.ex
@@ -1,41 +1,78 @@
 defprotocol Avro.Translator do
-  @spec translate_type(t) :: :avro.record_field()
-  def translate_type(t)
+  @spec type(t) :: :avro.avro_type()
+  def type(t)
+
+  @spec field(t) :: :avro.record_field()
+  def field(t)
+end
+
+defimpl Avro.Translator, for: Dictionary.Type.List do
+  def type(%{item_type: item_type} = t) do
+    struct(item_type, Map.from_struct(t))
+    |> Avro.Translator.type()
+    |> :avro_array.type()
+  end
+
+  def field(%{name: name} = t) do
+    :avro_record.define_field(name, type(t))
+  end
 end
 
 defimpl Avro.Translator, for: Dictionary.Type.Map do
-  def translate_type(%{name: name, dictionary: dictionary}) do
-    sub_fields = Enum.map(dictionary, &Avro.Translator.translate_type/1)
-    child_record = :avro_record.type("record_0", sub_fields)
-    union = :avro_union.type([:null, child_record])
-    :avro_record.define_field(name, union)
+
+  def type(%{dictionary: dictionary}) do
+    fields = Enum.map(dictionary, &Avro.Translator.field/1)
+    record = :avro_record.type("record_0", fields)
+    :avro_union.type([:null, record])
+  end
+
+  def field(%{name: name} = t) do
+    :avro_record.define_field(name, type(t))
   end
 end
 
 defimpl Avro.Translator, for: Dictionary.Type.String do
   @nullable_string :avro_union.type([:null, :string])
-  def translate_type(%{name: name}) do
+  def type(_) do
+    @nullable_string
+  end
+
+  def field(%{name: name}) do
     :avro_record.define_field(name, @nullable_string)
   end
 end
 
 defimpl Avro.Translator, for: Dictionary.Type.Integer do
   @nullable_long :avro_union.type([:null, :long])
-  def translate_type(%{name: name}) do
+
+  def type(_) do
+    @nullable_long
+  end
+
+  def field(%{name: name}) do
     :avro_record.define_field(name, @nullable_long)
   end
 end
 
 defimpl Avro.Translator, for: Dictionary.Type.Float do
   @nullable_double :avro_union.type([:null, :double])
-  def translate_type(%{name: name}) do
+
+  def type(_) do
+    @nullable_double
+  end
+
+  def field(%{name: name}) do
     :avro_record.define_field(name, @nullable_double)
   end
 end
 
 defimpl Avro.Translator, for: Dictionary.Type.Boolean do
   @nullable_boolean :avro_union.type([:null, :boolean])
-  def translate_type(%{name: name}) do
+  def type(_) do
+    @nullable_boolean
+  end
+
+  def field(%{name: name}) do
     :avro_record.define_field(name, @nullable_boolean)
   end
 end
--- apps/avro/test/avro/translator_test.exs
+++ apps/avro/test/avro/translator_test.exs
@@ -3,8 +3,8 @@ defmodule Avro.TranslatorTest do
   import Checkov
 
   data_test "primitive types" do
-    avro_type = Avro.Translator.translate_type(type)
-    schema = :avro_record.type("person", [avro_type])
+    avro_field = Avro.Translator.field(type)
+    schema = :avro_record.type("person", [avro_field])
 
     encoder = :avro.make_simple_encoder(schema, [])
     decoder = :avro.make_simple_decoder(schema, [])
@@ -38,8 +38,8 @@ defmodule Avro.TranslatorTest do
         ]
       )
 
-    avro_type = Avro.Translator.translate_type(type)
-    schema = :avro_record.type("person", [avro_type])
+    avro_field = Avro.Translator.field(type)
+    schema = :avro_record.type("person", [avro_field])
 
     encoder = :avro.make_simple_encoder(schema, [])
     decoder = :avro.make_simple_decoder(schema, [])
@@ -52,6 +52,64 @@ defmodule Avro.TranslatorTest do
       |> decoder.()
       |> Map.new()
       |> Map.update!("spouse", &Map.new/1)
+
+    assert result == input
+  end
+
+  test "converts simple list" do
+    type =
+      Dictionary.Type.List.new!(
+        name: "colors",
+        item_type: Dictionary.Type.String
+      )
+
+    avro_field = Avro.Translator.field(type)
+    schema = :avro_record.type("person", [avro_field])
+
+    encoder = :avro.make_simple_encoder(schema, [])
+    decoder = :avro.make_simple_decoder(schema, [])
+
+    input = %{"colors" => ["red", "blue"]}
+
+    result =
+      input
+      |> encoder.()
+      |> decoder.()
+      |> Map.new()
+
+    assert result == input
+  end
+
+  test "converts list of maps" do
+    type =
+      Dictionary.Type.List.new!(
+        name: "friends",
+        item_type: Dictionary.Type.Map,
+        dictionary: [
+          Dictionary.Type.String.new!(name: "name"),
+          Dictionary.Type.Integer.new!(name: "age")
+        ]
+      )
+
+    avro_field = Avro.Translator.field(type)
+    schema = :avro_record.type("person", [avro_field])
+
+    encoder = :avro.make_simple_encoder(schema, [])
+    decoder = :avro.make_simple_decoder(schema, [])
+
+    input = %{
+      "friends" => [
+        %{"name" => "bob", "age" => 21},
+        %{"name" => "fred", "age" => 22}
+      ]
+    }
+
+    result =
+      input
+      |> encoder.()
+      |> decoder.()
+      |> Map.new()
+      |> Map.update!("friends", fn l -> Enum.map(l, &Map.new/1) end)
 
     assert result == input
   end
