c34a3cca3b36fd87617aa530af79aa2f9d268996
Johnson Denen <johnson.denen@gmail.com>
2020-02-05 21:43:55 -0500

    Setup OTP releases for each service

--- mix.exs
+++ mix.exs
@@ -6,11 +6,42 @@ defmodule Hindsight.MixProject do
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      releases: releases(),
       aliases: aliases(),
       preferred_cli_env: ["test.unit": :test, "test.int": :test],
       dialyzer: [
         plt_add_apps: [:ex_unit, :eex],
         plt_file: {:no_warn, ".dialyzer/#{System.version()}.plt"}
+      ]
+    ]
+  end
+
+  defp releases do
+    [
+      orchestrate: [
+        version: {:from_app, :service_orchestrate},
+        include_executables_for: [:unix],
+        applications: [service_orchestrate: :permanent]
+      ],
+      gather: [
+        version: {:from_app, :service_gather},
+        include_executables_for: [:unix],
+        applications: [service_gather: :permanent]
+      ],
+      broadcast: [
+        version: {:from_app, :service_broadcast},
+        include_executables_for: [:unix],
+        applications: [service_broadcast: :permanent]
+      ],
+      persist: [
+        version: {:from_app, :service_persist},
+        include_executables_for: [:unix],
+        applications: [service_persist: :permanent]
+      ],
+      acquire: [
+        version: {:from_app, :service_acquire},
+        include_executables_for: [:unix],
+        applications: [service_acquire: :permanent]
       ]
     ]
   end
