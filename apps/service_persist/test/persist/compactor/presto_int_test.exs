e1764e8b29d2f08862c0869e4137b1b68d5bdb16
Brian Balser <brian.balser@gmail.com>
2020-02-12 15:01:53 -0500

    Implemented Persist.Compactor

---
+++ apps/service_persist/test/persist/compactor/presto_int_test.exs
@@ -0,0 +1,70 @@
+defmodule Persist.Compactor.PrestoIntTest do
+  use ExUnit.Case
+  use Divo
+  require Temp.Env
+
+  @prestige [
+    url: "http://localhost:8080",
+    user: "testing",
+    catalog: "hive",
+    schema: "default"
+  ]
+
+  Temp.Env.modify([
+    %{
+      app: :service_persist,
+      key: Persist.Compactor.Presto,
+      set: [
+        prestige: @prestige
+      ]
+    }
+  ])
+
+  @moduletag integration: true, divo: true
+
+  test "should compact a table in presto" do
+    persist =
+      Load.Persist.new!(
+        id: "persist-1",
+        dataset_id: "ds1",
+        subset_id: "sb1",
+        source: "topic-a",
+        destination: "table_a"
+      )
+
+    session = Prestige.new_session(@prestige)
+    {:ok, _} = Prestige.execute(session, "CREATE TABLE table_a(name varchar, age integer)")
+
+    1..10
+    |> Enum.each(fn _ ->
+      {:ok, _} =
+        Prestige.execute(session, "INSERT INTO table_a(name, age) values(#{generate_row()})")
+    end)
+
+    Prestige.execute!(session, "select * from table_a")
+    |> Prestige.Result.as_maps()
+
+    assert 10 == number_of_s3_files()
+
+    assert :ok == Persist.Compactor.Presto.compact(persist)
+
+    assert 1 == number_of_s3_files()
+  end
+
+  defp number_of_s3_files() do
+    ExAws.S3.list_objects("kdp-cloud-storage")
+    |> ExAws.request!()
+    |> (fn response -> response.body.contents end).()
+    |> Enum.map(&Map.get(&1, :key))
+    |> Enum.filter(&String.starts_with?(&1, "hive-s3/table_a/"))
+    |> length()
+  end
+
+  defp generate_row() do
+    "'#{random_string(10)}', #{:crypto.rand_uniform(0, 100)}"
+  end
+
+  defp random_string(length) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64() |> binary_part(0, length)
+  end
+end
---
+++ apps/service_persist/test/persist/compactor/presto_test.exs
@@ -0,0 +1,51 @@
+defmodule Persist.Compactor.PrestoTest do
+  use ExUnit.Case
+  use Placebo
+  require Temp.Env
+
+  @prestige [
+    url: "http://localhost:8080",
+    user: "testing",
+    catalog: "hive",
+    schema: "default"
+  ]
+
+  Temp.Env.modify([
+    %{
+      app: :service_persist,
+      key: Persist.Compactor.Presto,
+      set: [
+        prestige: @prestige
+      ]
+    }
+  ])
+
+  setup do
+    persist =
+      Load.Persist.new!(
+        id: "persist-1",
+        dataset_id: "ds1",
+        subset_id: "sb1",
+        source: "topic-a",
+        destination: "table_a"
+      )
+
+    [persist: persist]
+  end
+
+  test "will return error if prestige returns an error", %{persist: persist} do
+    allow Prestige.execute(any(), any()), return: {:error, "failure"}
+
+    assert {:error, "failure"} == Persist.Compactor.Presto.compact(persist)
+  end
+
+  test "will return error tuple if number of rows do not match", %{persist: persist} do
+    allow Prestige.execute(any(), any()), return: {:ok, :does_not_matter}
+    allow Prestige.execute(any(), starts_with("CREATE TABLE")), return: {:ok, %{rows: [[101]]}}
+    allow Prestige.execute(any(), "SELECT count(1) FROM table_a"), return: {:ok, %{rows: [[100]]}}
+
+    expected_reason = "Failed 'table_a' compaction: New count (100) did not match original count (101)"
+
+    assert {:error, expected_reason} == Persist.Compactor.Presto.compact(persist)
+  end
+end
