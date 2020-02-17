66b7c7475a90c02436c97e50cde50b43c27713da
Brian Balser <brian.balser@gmail.com>
2020-02-17 07:41:01 -0500

    Fixed issue in Retry Annotation with default parameter values

--- apps/annotated_retry/lib/annotated_retry.ex
+++ apps/annotated_retry/lib/annotated_retry.ex
@@ -20,7 +20,7 @@ defmodule Annotated.Retry do
       Module.put_attribute(env.module, :retry_funs, %{
         kind: kind,
         name: name,
-        args: args,
+        args: Enum.map(args, &de_underscore_name/1),
         guards: guards,
         retry_opts: Keyword.update(retry_opts, :with, [], &Enum.to_list/1)
       })
@@ -34,12 +34,11 @@ defmodule Annotated.Retry do
     Module.delete_attribute(env.module, :retry_funs)
 
     override_list =
-      Enum.map(retry_funs, fn %{name: name, args: args} ->
-        {name, length(args)}
-      end)
+      Enum.map(retry_funs, &gen_override_list/1)
+      |> List.flatten()
 
     overrides =
-      quote do
+      quote location: :keep do
         defoverridable unquote(override_list)
       end
 
@@ -52,8 +51,18 @@ defmodule Annotated.Retry do
     [overrides | functions]
   end
 
+  defp gen_override_list(%{name: name, args: args}) do
+    no_default_args_length =
+      Enum.reduce(args, 0, fn
+        {:\\, _, _}, acc -> acc
+        _, acc -> acc + 1
+      end)
+
+    Enum.map(no_default_args_length..length(args), fn i -> {name, i} end)
+  end
+
   defp gen_function(%{kind: :def, guards: [], name: name, args: args}, body) do
-    quote do
+    quote location: :keep do
       def unquote(name)(unquote_splicing(args)) do
         unquote(body)
       end
@@ -61,7 +70,7 @@ defmodule Annotated.Retry do
   end
 
   defp gen_function(%{kind: :def, guards: guards, name: name, args: args}, body) do
-    quote do
+    quote location: :keep do
       def unquote(name)(unquote_splicing(args)) when unquote_splicing(guards) do
         unquote(body)
       end
@@ -69,7 +78,7 @@ defmodule Annotated.Retry do
   end
 
   defp gen_function(%{kind: :defp, guards: [], name: name, args: args}, body) do
-    quote do
+    quote location: :keep do
       defp unquote(name)(unquote_splicing(args)) do
         unquote(body)
       end
@@ -77,7 +86,7 @@ defmodule Annotated.Retry do
   end
 
   defp gen_function(%{kind: :defp, guards: guards, name: name, args: args}, body) do
-    quote do
+    quote location: :keep do
       defp unquote(name)(unquote_splicing(args)) when unquote_splicing(guards) do
         unquote(body)
       end
@@ -85,9 +94,15 @@ defmodule Annotated.Retry do
   end
 
   defp gen_body(fun) do
-    quote do
+    args =
+      Enum.map(fun.args, fn
+        {:\\, _, [arg_name | _]} -> arg_name
+        arg -> arg
+      end)
+
+    quote location: :keep do
       Retry.retry_while unquote(fun.retry_opts) do
-        case super(unquote_splicing(fun.args)) do
+        case super(unquote_splicing(args)) do
           {:error, reason} = error ->
             Logger.info(fn ->
               "#{__MODULE__}: Retrying function #{unquote(fun.name)}: #{inspect(reason)}"
@@ -99,6 +114,20 @@ defmodule Annotated.Retry do
             {:halt, result}
         end
       end
+    end
+  end
+
+  defp de_underscore_name({:\\, context, [{name, name_context, name_args} | t]} = arg) do
+    case to_string(name) do
+      "_" <> real_name -> {:\\, context, [{String.to_atom(real_name), name_context, name_args} | t]}
+      _ -> arg
+    end
+  end
+
+  defp de_underscore_name({name, context, args} = arg) do
+    case to_string(name) do
+      "_" <> real_name -> {String.to_atom(real_name), context, args}
+      _ -> arg
     end
   end
 end
