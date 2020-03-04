dbeae2851c17c0b57ad1ba91891e49d04d594115
Johnson Denen <johnson.denen@gmail.com>
2020-03-04 10:36:03 -0500

    Default log level to WARN

--- config/releases.exs
+++ config/releases.exs
@@ -15,9 +15,12 @@ import Config
 #
 
 # Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+config :logger,
+  level: :warn,
+  console: [
+    format: "$time $metadata[$level] $message\n",
+    metadata: [:request_id]
+  ]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
