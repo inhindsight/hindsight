2d5abafaf3833b2b4b513470cc32b7e9a4bfee34
Brian Balser <brian.balser@gmail.com>
2020-03-17 12:11:00 -0400

    Created Profile.Update struct

---
+++ apps/definition_profile_update/lib/profile_update.ex
@@ -0,0 +1,36 @@
+defmodule Profile.Update do
+  use Definition, schema: Profile.Update.V1
+
+  @type t :: %__MODULE__{
+          version: integer,
+          dataset_id: String.t(),
+          subset_id: String.t(),
+          updated: String.t(),
+          stats: map()
+        }
+
+  defstruct version: 1,
+            dataset_id: nil,
+            subset_id: nil,
+            updated: nil,
+            stats: %{}
+
+  def on_new(update) do
+    Map.put(update, :updated, NaiveDateTime.utc_now() |> NaiveDateTime.to_iso8601())
+    |> Ok.ok()
+  end
+end
+
+defmodule Profile.Update.V1 do
+  use Definition.Schema
+
+  def s do
+    schema(%Profile.Update{
+      version: version(1),
+      dataset_id: required_string(),
+      subset_id: required_string(),
+      updated: required_string(),
+      stats: spec(is_map())
+    })
+  end
+end
