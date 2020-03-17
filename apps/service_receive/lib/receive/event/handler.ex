26a43ed835c1eb8474152eaa9f2e31f9755bb95f
Kenny Drobnack <kdrobnack@pillartechnology.com>
2020-03-17 11:37:44 -0400

    Handling deleting a dataset in the Receive app

--- apps/service_receive/lib/receive/event/handler.ex
+++ apps/service_receive/lib/receive/event/handler.ex
@@ -3,7 +3,7 @@ defmodule Receive.Event.Handler do
   require Logger
 
   alias Receive.SocketManager
-  import Events, only: [accept_start: 0, accept_end: 0]
+  import Events, only: [accept_start: 0, accept_end: 0, definition_delete: 0]
 
   def handle_event(%Brook.Event{type: accept_start(), data: %Accept{} = accept}) do
     Logger.debug(fn -> "#{__MODULE__}: Received event #{accept_start()}: #{inspect(accept)}" end)
@@ -21,6 +21,19 @@ defmodule Receive.Event.Handler do
     Receive.Accept.Store.delete(id, sid)
 
     Receive.Accept.Registry.whereis(:"#{accept.destination}_manager")
+    |> Receive.Accept.Supervisor.terminate_child()
+
+    :ok
+  end
+
+  def handle_event(%Brook.Event{type: definition_delete(), data: %Delete{} = delete}) do
+    Logger.debug(fn ->
+      "#{__MODULE__}: Received event #{definition_delete()}: #{inspect(delete)}"
+    end)
+
+    Receive.Accept.Store.delete(delete.dataset_id, delete.subset_id)
+
+    Receive.Accept.Registry.whereis(:"#{delete.destination}_manager")
     |> Receive.Accept.Supervisor.terminate_child()
 
     :ok
