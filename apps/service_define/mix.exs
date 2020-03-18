5239fb1d783ebb99976482fecae1be8ccdc78cfc
Nicholas Beach <nicholas.beach@accenture.com>
2020-03-18 15:23:34 -0400

    Remove more dead comments

--- apps/service_define/mix.exs
+++ apps/service_define/mix.exs
@@ -17,9 +17,6 @@ defmodule Define.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Define.Application, []},
@@ -27,13 +24,9 @@ defmodule Define.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.4.15"},
@@ -42,8 +35,6 @@ defmodule Define.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-
-      # Dev/Test Dependencies
       {:testing, in_umbrella: true, only: [:test]}
     ]
   end
