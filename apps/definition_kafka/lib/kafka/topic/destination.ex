8da6a438339615f5f3ed560c2c3e4c6cf027dddf
Johnson Denen <johnson.denen@gmail.com>
2020-03-24 11:30:54 -0400

    Move Elsa calls to handle_continue

--- apps/definition_kafka/lib/kafka/topic/destination.ex
+++ apps/definition_kafka/lib/kafka/topic/destination.ex
@@ -8,14 +8,6 @@ defmodule Kafka.Topic.Destination do
     |> Ok.map(&Map.put(dest, :pid, &1))
   end
 
-  @impl GenServer
-  def init(dest) do
-    with opts <- Map.from_struct(dest) |> Enum.into([]),
-         :ok <- Elsa.create_topic(dest.endpoints, dest.topic, opts) do
-      Ok.ok(dest)
-    end
-  end
-
   # TODO
   def write(_dest, _dictionary, _messages) do
     :ok
@@ -26,11 +18,25 @@ defmodule Kafka.Topic.Destination do
     :ok
   end
 
+  @spec delete(Destination.t()) :: :ok | {:error, term}
   def delete(dest) do
     with {:error, reason} <- Elsa.delete_topic(dest.endpoints, dest.topic),
          log_reason <- inspect(reason) do
       Logger.warn(fn -> "Topic '#{dest.topic}' failed to delete: #{log_reason}" end)
       Ok.error(reason)
+    end
+  end
+
+  @impl GenServer
+  def init(dest) do
+    {:ok, dest, {:continue, :init}}
+  end
+
+  @impl GenServer
+  def handle_continue(:init, dest) do
+    with opts <- Map.from_struct(dest) |> Enum.into([]),
+         :ok <- Elsa.create_topic(dest.endpoints, dest.topic, opts) do
+      {:noreply, dest}
     end
   end
 end
