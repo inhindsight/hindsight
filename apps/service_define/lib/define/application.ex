514d003aafdff8ec1815580497a181c478b252e8
Nicholas Beach <nicholascbeach@gmail.com>
2020-03-27 15:53:25 -0400

    Fixed brook configuraiton in define service

--- apps/service_define/lib/define/application.ex
+++ apps/service_define/lib/define/application.ex
@@ -1,28 +1,27 @@
 defmodule Define.Application do
   use Application
-  use Properties, otp_app: :service_define
+  use Properties, otp_app: :service_acquire
+
+  getter(:brook, required: true)
 
   def instance(), do: :define_instance
 
   def start(_type, _args) do
     children = [
-      DefineWeb.Endpoint,
-      brook()
+      start_brook(),
+      DefineWeb.Endpoint
     ]
 
     opts = [strategy: :one_for_one, name: Define.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
+  defp start_brook() do
+    {Brook, Keyword.put(brook(), :instance, instance())}
+  end
+
   def config_change(changed, _new, removed) do
     DefineWeb.Endpoint.config_change(changed, removed)
     :ok
-  end
-
-  defp brook() do
-    case get_config_value(:brook, required: true) do
-      nil -> []
-      config -> {Brook, Keyword.put(config, :instance, instance())}
-    end
   end
 end
