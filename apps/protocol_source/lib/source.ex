5d55e9747c35f35dc532a0a0239432e8dbad63b8
Brian Balser <brian.balser@gmail.com>
2020-03-30 15:13:28 -0400

    Fixed dialyzer warnings

--- apps/protocol_source/lib/source.ex
+++ apps/protocol_source/lib/source.ex
@@ -2,7 +2,7 @@ defprotocol Source do
   @spec start_link(t, Source.Context.t()) :: GenServer.on_start()
   def start_link(t, context)
 
-  @spec stop(t, GenServer.name()) :: :ok
+  @spec stop(t, pid | GenServer.name()) :: :ok
   def stop(t, server)
 
   @spec delete(t) :: :ok | {:error, term}
