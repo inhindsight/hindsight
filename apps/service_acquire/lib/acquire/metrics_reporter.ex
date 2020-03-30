cf56377b4335b0ff3e8acb9b31d838365d6bb3e0
jeffgrunewald <jeff@grunewalddesign.com>
2020-03-30 18:02:33 -0400

    run formatter

--- apps/service_acquire/lib/acquire/metrics_reporter.ex
+++ apps/service_acquire/lib/acquire/metrics_reporter.ex
@@ -3,14 +3,15 @@ defmodule Acquire.MetricsReporter do
 
   @impl MetricsReporter
   def metrics() do
-    super() ++ [
-      last_value("phoenix.endpoint.start.time"),
-      last_value("phoenix.router.dispatch.start"),
-      summary("phoenix.endpoint.stop.duration", unit: {:native, :millisecond}),
-      summary("phoenix.router_dispatch.stop.duration", unit: {:native, :millisecond}),
-      summary("phoenix.socket_connected.duration", unit: {:native, :millisecond}),
-      summary("phoenix.channel_joined.duration", unit: {:native, :millisecond}),
-      summary("phoenix.channel_handled_in.duration", unit: {:native, :millisecond})
-    ]
+    super() ++
+      [
+        last_value("phoenix.endpoint.start.time"),
+        last_value("phoenix.router.dispatch.start"),
+        summary("phoenix.endpoint.stop.duration", unit: {:native, :millisecond}),
+        summary("phoenix.router_dispatch.stop.duration", unit: {:native, :millisecond}),
+        summary("phoenix.socket_connected.duration", unit: {:native, :millisecond}),
+        summary("phoenix.channel_joined.duration", unit: {:native, :millisecond}),
+        summary("phoenix.channel_handled_in.duration", unit: {:native, :millisecond})
+      ]
   end
 end
