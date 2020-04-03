73abb6cdce0fd1c2ea83a0d7d259fcdaf7354734
jeffgrunewald <jeff@grunewalddesign.com>
2020-04-03 10:36:42 -0400

    fixing port conflict for profile

--- apps/service_profile/config/test.exs
+++ apps/service_profile/config/test.exs
@@ -21,4 +21,4 @@ config :service_profile, Profile.Application,
     dispatcher: Brook.Dispatcher.Noop
   ]
 
-config :service_persist, Persist.MetricsReporter, port: 9574
+config :service_persist, Persist.MetricsReporter, port: 9575
