b4824c22433fcb9e8a17b92877f905430fa75a43
Johnson Denen <johnson.denen@gmail.com>
2020-04-09 14:20:10 -0400

    Document protocol_destination

--- apps/protocol_transform_step/lib/transform/step.ex
+++ apps/protocol_transform_step/lib/transform/step.ex
@@ -1,4 +1,8 @@
 defprotocol Transform.Step do
+  @moduledoc """
+  Defines a protocol for transforming fields in a `Dictionary`.
+  """
+
   @spec transform_dictionary(step :: t, dictionary :: Dictionary.t()) ::
           {:ok, Dictionary.t()} | {:error, term}
   def transform_dictionary(step, dictionary)
