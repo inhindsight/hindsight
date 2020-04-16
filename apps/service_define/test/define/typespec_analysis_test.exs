df5e23ca0184cb2b8468192952d753ee1cd86878
Nathaniel Scott Stevens <nstevens@pillartechnology.com>
2020-04-16 16:01:47 -0400

    Update apps/service_define/test/define/typespec_analysis_test.exs
    Co-Authored-By: Johnson Denen <johnson.denen@gmail.com>

--- apps/service_define/test/define/typespec_analysis_test.exs
+++ apps/service_define/test/define/typespec_analysis_test.exs
@@ -2,22 +2,24 @@ defmodule Define.TypespecAnalysisTest do
   use ExUnit.Case
   alias Define.{TypespecAnalysis}
 
-  test "get_types() returns all types from the typespec" do
-    expected = %{
-      "integer_type" => "integer",
-      "string_type" => "string",
-      "atom_type" => "atom",
-      "float_type" => "float",
-      "string_list_type" => {"list", "string"},
-      "integer_list_type" => {"list", "integer"},
-      "map_type" => "map",
-      "dictionary_type" => "dictionary"
-    }
+  describe "get_types/1" do
+    test "returns all types from the typespec" do
+      expected = %{
+        "integer_type" => "integer",
+        "string_type" => "string",
+        "atom_type" => "atom",
+        "float_type" => "float",
+        "string_list_type" => {"list", "string"},
+        "integer_list_type" => {"list", "integer"},
+        "map_type" => "map",
+        "dictionary_type" => "dictionary"
+      }
 
-    assert expected == TypespecAnalysis.get_types(TypespecAnalysisTestModule)
-  end
+      assert expected == TypespecAnalysis.get_types(TypespecAnalysisTestModule)
+    end
 
-  test "returns empty map when a typespec is not defined" do
-    assert %{} == TypespecAnalysis.get_types(TypespecAnalysisTestModuleNoSpec)
+    test "returns empty map when a typespec is not defined" do
+      assert %{} == TypespecAnalysis.get_types(TypespecAnalysisTestModuleNoSpec)
+    end
   end
 end
